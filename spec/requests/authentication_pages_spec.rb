5a8de48d0b95e85d461070b7169c0f331bc9ee77
Dele <oladele@gmail.com>
2012-12-28 14:28:43 -0500

    Complete Ch9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,10 +31,13 @@ describe "Authentication" do
 	  	before { sign_in user }
 
   		it { should have_selector('title', text: user.name) }
-  		it { should have_link('Profile', href: user_path(user)) }
+  		
+      it { should have_link('Users', href: users_path) }
+      it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
   		it { should have_link('Sign out', href: signout_path) }
-  		it { should_not have_link('Sign in', href: signin_path) }
+  		
+      it { should_not have_link('Sign in', href: signin_path) }
   		describe "followed by signout" do
   			before { click_link "Sign out" }
   			it { should have_link('Sign in') }
@@ -62,7 +65,7 @@ describe "Authentication" do
           end
         end
       end
-      
+
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -74,7 +77,13 @@ describe "Authentication" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
       end
+
     end
 
     describe "as wrong user" do
@@ -90,6 +99,18 @@ describe "Authentication" do
       describe "submitting a PUT request to the Users#update action" do
         before { put user_path(wrong_user) }
         specify { response.should redirect_to(root_path) }  
+      end
+    end
+
+    describe "as non-admin user" do
+      let(:user) {FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }
       end
     end
   end
