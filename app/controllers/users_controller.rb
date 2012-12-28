4141dfab790d92a0476051966f1598812914ad08
Dele <oladele@gmail.com>
2012-12-28 11:58:29 -0500

    Begin 9.3 Showing all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+  before_filter :signed_in_user,  only: [:edit, :update]
+  before_filter :correct_user,    only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
@@ -20,15 +22,32 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
+    #per section 9.2.2. Above no longer needed since addition of before_filter :correct_user
   end
 
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
+    #per section 9.2.2. Above no longer needed since addition of before_filter :correct_user
     if @user.update_attributes(params[:user])
-      #Handle a successful update
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end
   end
+
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
