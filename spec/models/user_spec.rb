a23a17a1648b7d456ed7d9979eca8a70bde3a3fe
Dele <oladele@gmail.com>
2012-12-29 20:24:42 -0500

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,6 +33,7 @@ describe User do
 	it { should_not be_admin }
 
 	it { should respond_to(:microposts) }
+	it { should respond_to(:feed) }
 
 	describe "with admin attribute set to 'true'" do
 		before do
@@ -146,7 +147,7 @@ describe User do
 			FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
 		end
 		let!(:newer_micropost) do
-			FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
 		end
 
 		it "should have the right microposts in the right order" do
@@ -160,6 +161,16 @@ describe User do
 			microposts.each do |micropost|
 				Micropost.find_by_id(micropost.id).should be_nil
 			end
+		end
+
+		describe "status" do
+			let(:unfollowed_post) do
+				FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+			end
+
+			its(:feed) { should include(newer_micropost) }
+			its(:feed) { should include(older_micropost) }
+			its(:feed) {should_not include(unfollowed_post) }
 		end
 	end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -28,35 +28,50 @@ describe "Static pages" do
         it_should_behave_like "all static pages"
     end
 
-  describe "About page" do
-      before { visit about_path }
-      let(:heading) { 'About Us' }
-      let(:page_title) {'About Us'}
+    describe "About page" do
+        before { visit about_path }
+        let(:heading) { 'About Us' }
+        let(:page_title) {'About Us'}
 
-      it_should_behave_like "all static pages"
-  end
+        it_should_behave_like "all static pages"
+    end
 
-  describe "Contact" do
-    before { visit contact_path }
-      let(:heading) { 'Contact' }
-      let(:page_title) {'Contact'}
+    describe "Contact" do
+      before { visit contact_path }
+        let(:heading) { 'Contact' }
+        let(:page_title) {'Contact'}
 
-      it_should_behave_like "all static pages"
-  end
+        it_should_behave_like "all static pages"
+    end
 
-  it "should have the right links on the layout" do
-    visit root_path
-    click_link "About"
-    page.should have_selector 'title', text: full_title('About Us')
-    click_link "Help"
-    page.should have_selector 'title', text: full_title('Help')
-    click_link "Contact"
-    page.should have_selector 'title', text: full_title('Contact')
-    click_link "Home"
-    click_link "Sign up now!"
-    page.should have_selector 'title', text: full_title('Sign Up')
-    click_link "sample app"
-    page.should have_selector 'title', text: full_title('')
-  end
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      page.should have_selector 'title', text: full_title('About Us')
+      click_link "Help"
+      page.should have_selector 'title', text: full_title('Help')
+      click_link "Contact"
+      page.should have_selector 'title', text: full_title('Contact')
+      click_link "Home"
+      click_link "Sign up now!"
+      page.should have_selector 'title', text: full_title('Sign Up')
+      click_link "sample app"
+      page.should have_selector 'title', text: full_title('')
+    end
 
+    describe "for signed-in users" do
+      let(:user){ FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
 end
