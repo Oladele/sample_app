77e061af0a4749c4460db31a40b1f79a91b038e2
Dele <oladele@gmail.com>
2013-07-06 14:53:10 -0400

    Implemented follow button. All test passing

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "UserPages" do
 
-	subject { page }
+    subject { page }
 
     describe "index" do
 
@@ -49,16 +49,16 @@ describe "UserPages" do
         end
     end
 
-	describe "profile page" do
+    describe "profile page" do
 
-		let(:user) { FactoryGirl.create(:user) }
+        let(:user) { FactoryGirl.create(:user) }
         let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
         let!(:m2) {FactoryGirl.create(:micropost, user: user, content: "Bar") }
-	  	
+        
         before { visit user_path(user) }
 
-	  	it { should have_selector('h1',    text: user.name) }
-	  	it { should have_selector('title', text: user.name) }
+        it { should have_selector('h1',    text: user.name) }
+        it { should have_selector('title', text: user.name) }
 
         describe "microposts" do
             it { should have_content(m1.content) }
@@ -117,106 +117,56 @@ describe "UserPages" do
         end
         #END
 
-        describe "follow/unfollow buttons" do
-      let(:other_user) { FactoryGirl.create(:user) }
-      before { sign_in user }
-
-      describe "following a user" do
-        before { visit user_path(other_user) }
-
-        it "should increment the followed user count" do
-          expect do
-            click_button "Follow"
-          end.to change(user.followed_users, :count).by(1)
-        end
-
-        it "should increment the other user's followers count" do
-          expect do
-            click_button "Follow"
-          end.to change(other_user.followers, :count).by(1)
-        end
-
-        describe "toggling the button" do
-          before { click_button "Follow" }
-          it { should have_selector('input', value: 'Unfollow') }
-        end
-      end
-
-    describe "unfollowing a user" do
-    before do
-      user.follow!(other_user)
-      visit user_path(other_user)
     end
 
-    it "should decrement the followed user count" do
-      expect do
-        click_button "Unfollow"
-      end.to change(user.followed_users, :count).by(-1)
-    end
-
-    it "should decrement the other user's followers count" do
-      expect do
-        click_button "Unfollow"
-      end.to change(other_user.followers, :count).by(-1)
-    end
-
-    describe "toggling the button" do
-      before { click_button "Unfollow" }
-      it { should have_selector('input', value: 'Follow') }
-    end
-    end
+    describe "signup page" do
+        before  { visit signup_path }
+        
+        it { should have_selector('h1', text: 'Sign Up') }
+        it { should have_selector('title', text: full_title('Sign Up'))}
     end
         
+    describe "signup" do
+        before  { visit signup_path }
+        
+        let(:submit) { "Create my account" }
 
-        describe "signup page" do
-            before  { visit signup_path }
-            
-            it { should have_selector('h1', text: 'Sign Up') }
-            it { should have_selector('title', text: full_title('Sign Up'))}
+        describe "with invalid information" do
+            it "should not create a user" do
+                expect { click_button submit }.not_to change(User, :count)      
+            end
+
+            describe "after submission" do
+                before { click_button submit}
+                it { should have_selector('title', text: 'Sign Up') }
+                it { should have_content('error') }
+                it { should have_content('Password can\'t be blank') }
+                it { should have_content('Name can\'t be blank') }
+                it { should have_content('Email can\'t be blank') }
+                it { should have_content('Email is invalid') }
+                it { should have_content('Password is too short') }
+                it { should have_content('Password confirmation can\'t be blank') }
+            end
         end
-    		
-    	describe "signup" do
-    		before	{ visit signup_path }
-        	
-        	let(:submit) { "Create my account" }
 
-        	describe "with invalid information" do
-        		it "should not create a user" do
-        			expect { click_button submit }.not_to change(User, :count)		
-        		end
-
-                describe "after submission" do
-                    before { click_button submit}
-                    it { should have_selector('title', text: 'Sign Up') }
-                    it { should have_content('error') }
-                    it { should have_content('Password can\'t be blank') }
-                    it { should have_content('Name can\'t be blank') }
-                    it { should have_content('Email can\'t be blank') }
-                    it { should have_content('Email is invalid') }
-                    it { should have_content('Password is too short') }
-                    it { should have_content('Password confirmation can\'t be blank') }
-                end
-        	end
-
-        	describe "with valid information" do
-        		before	do
-        			fill_in "Name", 		with: "Example User"
-        			fill_in "Email",		with: "user@example.com"
-        			fill_in "Password",		with: "foobar"
-        			fill_in "Confirmation",	with:"foobar"
-        		end
-        		it "should create a user" do
-        			expect { click_button submit }.to change(User, :count).by(1)
-        		end
-                describe "after saving the user" do
-                    before { click_button submit }
-                    let(:user){ User.find_by_email('user@example.com') }
-                    
-                    it { should have_selector( 'title', text: user.name) }
-                    it { should have_selector( 'div.alert.alert-success', text: 'Welcome')}
-                    it { should have_link('Sign out') }
-                end
-        	end
+        describe "with valid information" do
+            before  do
+                fill_in "Name",         with: "Example User"
+                fill_in "Email",        with: "user@example.com"
+                fill_in "Password",     with: "foobar"
+                fill_in "Confirmation", with:"foobar"
+            end
+            it "should create a user" do
+                expect { click_button submit }.to change(User, :count).by(1)
+            end
+            describe "after saving the user" do
+                before { click_button submit }
+                let(:user){ User.find_by_email('user@example.com') }
+                
+                it { should have_selector( 'title', text: user.name) }
+                it { should have_selector( 'div.alert.alert-success', text: 'Welcome')}
+                it { should have_link('Sign out') }
+            end
         end
     end
 
